e6b40df4cc6d5ea2d9f1a2605915d8925768188f
cty288 <tc3158@nyu.edu>
2023-04-21 01:48:16 -0400

    update

--- RNNModel.py
+++ RNNModel.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-
+import torch.nn.functional as F
 
 
 class RNNModel(nn.Module):
@@ -26,7 +26,11 @@ class RNNModel(nn.Module):
 
         return answer_start_prob, no_answer_prob
     
-    
+
+def one_hot(tensor, num_classes):
+    one_hot_tensor = torch.zeros(tensor.size(0), num_classes, device=tensor.device)
+    one_hot_tensor.scatter_(1, tensor.unsqueeze(1), 1)
+    return one_hot_tensor
 
 def train(model, dataloader, answer_criterion, no_answer_criterion, optimizer, device, num_epochs):
     model.train()
@@ -38,7 +42,14 @@ def train(model, dataloader, answer_criterion, no_answer_criterion, optimizer, d
 
             no_answer_target = (answer_start == -1).float().unsqueeze(1).to(device)  # Add a dimension to match no_answer_prob's shape
             answer_start = answer_start.clamp(min=0, max=article.size(1) - 1)  # Clamp answer_start to avoid out-of-bounds error
-            loss = answer_criterion(answer_start_prob, answer_start.squeeze().to(device)) + no_answer_criterion(no_answer_prob.squeeze(), no_answer_target.squeeze())
+            
+            
+            answer_start_one_hot = one_hot(answer_start.squeeze().to(device), article.size(1))
+            loss = F.mse_loss(answer_start_prob, answer_start_one_hot) + no_answer_criterion(no_answer_prob.squeeze(), no_answer_target.squeeze())
+
+            #loss = F.mse_loss(answer_start_prob, answer_start.squeeze().to(device)) + no_answer_criterion(no_answer_prob.squeeze(), no_answer_target.squeeze())
+
+            #loss = answer_criterion(answer_start_prob, answer_start.squeeze().to(device)) + no_answer_criterion(no_answer_prob.squeeze(), no_answer_target.squeeze())
             epoch_loss += loss.item()
 
             loss.backward()
@@ -46,8 +57,7 @@ def train(model, dataloader, answer_criterion, no_answer_criterion, optimizer, d
         print(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {epoch_loss / len(dataloader):.4f}")
 
         
-
-        
+      
         
 
 def test(model, dataloader, answer_criterion, no_answer_criterion, device):
@@ -56,6 +66,9 @@ def test(model, dataloader, answer_criterion, no_answer_criterion, device):
     correct_answer_start = 0
     correct_no_answer = 0
     total = 0
+    
+    true_start_positions = []
+    predicted_start_positions = []
 
     with torch.no_grad():
         for article, question, answer_start in dataloader:
@@ -65,7 +78,12 @@ def test(model, dataloader, answer_criterion, no_answer_criterion, device):
 
             no_answer_target = (answer_start == -1).float().unsqueeze(1).to(device)  # Add a dimension to match no_answer_prob's shape
             answer_start = answer_start.clamp(min=0, max=article.size(1) - 1)  # Clamp answer_start to avoid out-of-bounds error
-            answer_loss = answer_criterion(answer_start_prob, answer_start.squeeze().to(device))
+            #answer_loss = answer_criterion(answer_start_prob, answer_start.squeeze().to(device))
+            
+            answer_start_one_hot = one_hot(answer_start.squeeze().to(device), article.size(1))
+            answer_loss = F.mse_loss(answer_start_prob, answer_start_one_hot)
+            
+            #answer_loss = F.mse_loss(answer_start_prob, answer_start.squeeze().to(device))
             no_answer_loss = no_answer_criterion(no_answer_prob.squeeze(), no_answer_target.squeeze())
             loss = answer_loss + no_answer_loss
             test_loss += loss.item()
@@ -78,12 +96,18 @@ def test(model, dataloader, answer_criterion, no_answer_criterion, device):
             # Only consider answer_start predictions for samples with actual answers
             has_answer = answer_start != 0
             correct_answer_start += (pred_start_pos[has_answer] == answer_start[has_answer]).sum().item()
+            
+            true_start_positions.extend(answer_start.tolist())
+            predicted_start_positions.extend(pred_start_pos.tolist())
+
 
     test_loss /= total
     accuracy_no_answer = correct_no_answer / total
     accuracy_answer_start = correct_answer_start / has_answer.sum().item()
+    
 
-    return test_loss, accuracy_no_answer, accuracy_answer_start
+
+    return test_loss, accuracy_no_answer, accuracy_answer_start, true_start_positions, predicted_start_positions
 
 
 
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from QAModel import *
+from RNNModel import *
 import numpy as np
 from torch.utils.data import DataLoader, TensorDataset
 import torch
@@ -7,6 +7,8 @@ import json
 from tfidf_generator import TFIDFGenerator
 import gensim.downloader as api
 from word2vec_generator import Word2VecGenerator
+import torch.nn.functional as F
+from sklearn.metrics import f1_score
 
 word2vec_model = api.load("word2vec-google-news-300")
 
@@ -16,28 +18,48 @@ def read_json_data(file_path, limit=None):
     json_obj = json.loads(json_str)
     return json_obj[:limit]
 
+#评估函数
+
+def evaluate_with_tolerance(true_start_positions, predicted_start_positions, tolerance=1):
+    within_tolerance = 0
+    for true_pos, pred_pos in zip(true_start_positions, predicted_start_positions):
+        if abs(true_pos - pred_pos) <= tolerance:
+            within_tolerance += 1
+    return within_tolerance / len(true_start_positions)
+
+
+def exact_match(y_true, y_pred):
+    return sum([1 if pred == true else 0 for pred, true in zip(y_pred, y_true)]) / len(y_true)
+
+def calculate_f1(y_true, y_pred):
+    return f1_score(y_true, y_pred, average='micro') 
+
+
+#读Json，dev懒得读了
 train_set = read_json_data("train_set_splited.json")
 test_set = read_json_data("test_set_splited.json")
 
 
+#三个model调用获得vector
 train_articles, train_questions, train_answer_starts, train_answers = Word2VecGenerator(train_set, word2vec_model).vectorize() #TFIDFGenerator(train_set).vectorize()
 test_articles, test_questions, test_answer_starts, train_answers = Word2VecGenerator(train_set, word2vec_model).vectorize() #TFIDFGenerator(test_set).vectorize()
 
 
-
+#不懂，GPT写的，应该是转换成tensor
 train_answer_starts = torch.tensor(train_answer_starts, dtype=torch.long)
 test_answer_starts = torch.tensor(test_answer_starts, dtype=torch.long)
 
-
+#不懂，GPT写的，应该和上面差不多
 train_dataloader = DataLoader(TensorDataset(train_articles, train_questions, train_answer_starts), batch_size=32)
 test_dataloader = DataLoader(TensorDataset(test_articles, test_questions, test_answer_starts), batch_size=32)
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+#RNN参数
 input_size = 300
-hidden_size = 512
-num_layers = 1
+hidden_size = 256
+num_layers = 2
 model = RNNModel(input_size, hidden_size, num_layers)
 criterion = nn.CrossEntropyLoss()
 no_answer_criterion = nn.BCEWithLogitsLoss()
@@ -47,8 +69,19 @@ model.to(device)
 
 # Train the model
 num_epochs = 10
+
+#开跑！
 train(model, train_dataloader, criterion, no_answer_criterion, optimizer, device, num_epochs)
 
 # Test the model
-test_loss, accuracy_no_answer, accuracy_answer_start = test(model, test_dataloader, criterion, no_answer_criterion, device)
+test_loss, accuracy_no_answer, accuracy_answer_start, true_start_positions, predicted_start_positions = test(model, test_dataloader, criterion, no_answer_criterion, device)
 print(f"Test Loss: {test_loss:.4f}, No Answer Accuracy: {accuracy_no_answer:.4f}, Answer Start Accuracy: {accuracy_answer_start:.4f}")
+
+accuracy_within_tolerance = evaluate_with_tolerance(true_start_positions, predicted_start_positions, tolerance=5)
+print(f"Accuracy within tolerance: {accuracy_within_tolerance:.4f}")
+
+
+
+exact_match_score = exact_match(true_start_positions, predicted_start_positions)
+f1_score_result = calculate_f1(true_start_positions, predicted_start_positions)
+print(f"Exact Match: {exact_match_score:.4f}, F1 Score: {f1_score_result:.4f}")
